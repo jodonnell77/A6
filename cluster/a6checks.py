078f8b055d38d8d7dcf42f9a11314cb959215b7c
Anthony Nguyen <anthony@localhost.localdomain>
2019-04-11 17:04:45 -0400

    ants message

--- cluster/a6checks.py
+++ cluster/a6checks.py
@@ -5,7 +5,7 @@ This file contains the functions for enforcing preconditions for k-means cluster
 We have written the first for you.  You will probably want to write others.
 
 John O'Donnell (jro79) and Anthony Nguyen (an523)
-4/11/19
+4/11/19 asdapsdkaspodkasdkapsdkaop
 """
 import math
 import random
@@ -15,7 +15,7 @@ import numpy
 def is_point(value):
     """
     Returns True if value is a list of int or float
-    
+
     Parameter value: a value to check
     Precondition: value can be anything
     """
@@ -26,9 +26,9 @@ def is_point(value):
 def is_point_list(value):
     """
     Returns True if value is a 2d list of int or float
-    
+
     This function also checks that all points in value have same dimension.
-    
+
     Parameter value: a value to check
     Precondition: value can be anything
     """
@@ -38,13 +38,13 @@ def is_point_list(value):
 def is_seed_list(value, k, size):
     """
     Returns True if value is k-element list of indices between 0 and (len - 1).
-    
+
     Parameter value: a value to check
     Precondition: value can be anything
-    
+
     Parameter k: The required list size
     Precondition: k is an int > 0
-    
+
     Paramater size: The database size
     Precondition: size is an int > 0
     """
